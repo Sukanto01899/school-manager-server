d9d148ebc18f849332a684feb91ded04a4aaa0ec
Sukanto Das <Suknto01899>
2023-02-06 17:26:17 +0600

    end point first

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res)=>{
-    res.send('Hello World Schoole Manegment Site')
-})
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.hcglgi5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const database = client.db('school-manager');
+        const students = database.collection('students');
+        const teachers = database.collection('teachers');
+        const departments = database.collection('departments');
+        const subjects = database.collection('subjects');
+        const books = database.collection('books');
+        const blogs = database.collection('blogs');
+
+        app.post('/student', (req, res)=>{
+            const student = req.body;
+        })
+
+        app.get('/students', async (req, res)=>{
+            const query = {};
+            const cursor = students.find(query);
+            const all_students = await cursor.toArray()
+            console.log('jkfs')
+            res.send(all_students)
+        })
+    }
+    finally{}
+}
+run().catch(console.dir)
+
 
 app.listen(port, ()=>{
     console.log('Success')
