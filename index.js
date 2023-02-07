5f47862d55571316ea6e18d07f5dfb995114a362
Sukanto Das <Suknto01899>
2023-02-07 11:49:48 +0600

    api ready

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.hcglgi5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -23,10 +22,10 @@ async function run(){
         const books = database.collection('books');
         const blogs = database.collection('blogs');
 
+        // Student
         app.post('/student',async (req, res)=>{
             const student = req.body;
             const result = await students.insertOne(student)
-            console.log(result)
             res.send(result)
         })
 
@@ -35,6 +34,76 @@ async function run(){
             const cursor = students.find(query);
             const all_students = await cursor.toArray()
             res.send(all_students)
+        })
+
+        // Teacher
+        app.post('/teacher',async (req, res)=>{
+            const teacher = req.body;
+            const result = await teachers.insertOne(teacher)
+            res.send(result)
+        })
+
+        app.get('/teachers',async (req, res)=>{
+            const query = {};
+            const cursor = teachers.find(query);
+            const all_teachers = await cursor.toArray()
+            res.send(all_teachers)
+        })
+
+        // department
+        app.post('/department',async (req, res)=>{
+            const department = req.body;
+            const result = await departments.insertOne(department)
+            res.send(result)
+        })
+
+        app.get('/departments',async (req, res)=>{
+            const query = {};
+            const cursor = departments.find(query);
+            const all_departments = await cursor.toArray()
+            res.send(all_departments)
+        })
+
+        // subjects
+        app.post('/subject',async (req, res)=>{
+            const subject = req.body;
+            const result = await subjects.insertOne(subject)
+            res.send(result)
+        })
+
+        app.get('/subjects',async (req, res)=>{
+            const query = {};
+            const cursor = subjects.find(query);
+            const all_subjects = await cursor.toArray()
+            res.send(all_subjects)
+        })
+
+        // books
+        app.post('/book',async (req, res)=>{
+            const book = req.body;
+            const result = await books.insertOne(book)
+            res.send(result)
+        })
+
+        app.get('/books',async (req, res)=>{
+            const query = {};
+            const cursor = books.find(query);
+            const all_books = await cursor.toArray()
+            res.send(all_books)
+        })
+
+        // blogs
+        app.post('/blog',async (req, res)=>{
+            const blog = req.body;
+            const result = await blogs.insertOne(blog)
+            res.send(result)
+        })
+
+        app.get('/blogs',async (req, res)=>{
+            const query = {};
+            const cursor = blogs.find(query);
+            const all_blogs = await cursor.toArray()
+            res.send(all_blogs)
         })
     }
     finally{}
